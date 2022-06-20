8ab83bb6fda0b387d845e16f67893f8e1b373165
nutti <nutti.metro@gmail.com>
2022-06-20 14:56:38 +0900

    Fix: lint error

--- MultipleCaseBranch/Source/MultipleCaseBranch/Private/SGraphNodeMultipleCaseBranch.cpp
+++ MultipleCaseBranch/Source/MultipleCaseBranch/Private/SGraphNodeMultipleCaseBranch.cpp
@@ -11,7 +11,9 @@
 class SGraphPinMultipleCaseBranchDefaultCaseExec : public SGraphPinExec
 {
 public:
-	SLATE_BEGIN_ARGS(SGraphPinMultipleCaseBranchDefaultCaseExec) {}
+	SLATE_BEGIN_ARGS(SGraphPinMultipleCaseBranchDefaultCaseExec)
+	{
+	}
 	SLATE_END_ARGS()
 
 	void Construct(const FArguments& InArgs, UEdGraphPin* InPin)
@@ -34,11 +36,7 @@ void SGraphNodeMultipleCaseBranch::CreatePinWidgets()
 	UK2Node_MultipleCaseBranch* MultipleCaseBranch = CastChecked<UK2Node_MultipleCaseBranch>(GraphNode);
 	UEdGraphPin* DefaultPin = MultipleCaseBranch->GetDefaultExecPin();
 
-	RightNodeBox->AddSlot()
-		.AutoHeight()
-		[
-			SNew(STextBlock).LineHeightPercentage(2.0f)
-		];
+	RightNodeBox->AddSlot().AutoHeight()[SNew(STextBlock).LineHeightPercentage(2.0f)];
 
 	for (auto It = GraphNode->Pins.CreateConstIterator(); It; ++It)
 	{
@@ -58,11 +56,7 @@ void SGraphNodeMultipleCaseBranch::CreatePinWidgets()
 			.AutoHeight()
 			.HAlign(HAlign_Right)
 			.VAlign(VAlign_Center)
-			.Padding(1.0f)
-			[
-				SNew(SImage)
-				.Image(FEditorStyle::GetBrush("Graph.Pin.DefaultPinSeparator"))
-			];
+			.Padding(1.0f)[SNew(SImage).Image(FEditorStyle::GetBrush("Graph.Pin.DefaultPinSeparator"))];
 
 		TSharedPtr<SGraphPin> NewPin = SNew(SGraphPinMultipleCaseBranchDefaultCaseExec, DefaultPin);
 		this->AddPin(NewPin.ToSharedRef());
@@ -71,21 +65,14 @@ void SGraphNodeMultipleCaseBranch::CreatePinWidgets()
 
 void SGraphNodeMultipleCaseBranch::CreateOutputSideAddButton(TSharedPtr<SVerticalBox> OutputBox)
 {
-	TSharedRef<SWidget> AddPinButton = AddPinButtonContent(
-		NSLOCTEXT("MultipleCaseBranchNode", "MultipleCaseBranchNodeAddPinButton", "Add pin"),
-		NSLOCTEXT("MultipleCaseBranchNode", "MultipleCaseBranchNodeAddPinButton_Tooltip", "Add new pin")
-	);
+	TSharedRef<SWidget> AddPinButton =
+		AddPinButtonContent(NSLOCTEXT("MultipleCaseBranchNode", "MultipleCaseBranchNodeAddPinButton", "Add pin"),
+			NSLOCTEXT("MultipleCaseBranchNode", "MultipleCaseBranchNodeAddPinButton_Tooltip", "Add new pin"));
 
 	FMargin AddPinPadding = Settings->GetOutputPinPadding();
 	AddPinPadding.Top += 6.0f;
 
-	OutputBox->AddSlot()
-		.AutoHeight()
-		.VAlign(VAlign_Center)
-		.Padding(AddPinPadding)
-		[
-			AddPinButton
-		];
+	OutputBox->AddSlot().AutoHeight().VAlign(VAlign_Center).Padding(AddPinPadding)[AddPinButton];
 }
 
 EVisibility SGraphNodeMultipleCaseBranch::IsAddPinButtonVisible() const
--- MultipleCaseBranch/Source/MultipleCaseBranch/Public/SGraphNodeMultipleCaseBranch.h
+++ MultipleCaseBranch/Source/MultipleCaseBranch/Public/SGraphNodeMultipleCaseBranch.h
@@ -6,7 +6,9 @@ class UK2Node_MultipleCaseBranch;
 
 class SGraphNodeMultipleCaseBranch : public SGraphNodeK2Base
 {
-	SLATE_BEGIN_ARGS(SGraphNodeMultipleCaseBranch){}
+	SLATE_BEGIN_ARGS(SGraphNodeMultipleCaseBranch)
+	{
+	}
 	SLATE_END_ARGS()
 
 	void Construct(const FArguments& InArgs, UK2Node_MultipleCaseBranch* InNode);
