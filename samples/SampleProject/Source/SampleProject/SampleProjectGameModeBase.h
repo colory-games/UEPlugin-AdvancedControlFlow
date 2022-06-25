33d1912735daa6fe0ad08be773d0ee2cec48abf5
nutti <nutti.metro@gmail.com>
2022-06-25 21:21:05 +0900

    Add: Sample project

---
+++ samples/SampleProject/Source/SampleProject/SampleProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SampleProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API ASampleProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
