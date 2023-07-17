1688469f42cfaffdeec6ffa9b7df830093962732
nutti <nutti.metro@gmail.com>
2023-07-17 16:43:10 +0900

    Add: Functional Test

---
+++ tests/functional_test/FunctionalTest/Source/FunctionalTest/FunctionalTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+
+#include "FunctionalTestGameModeBase.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class FUNCTIONALTEST_API AFunctionalTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
